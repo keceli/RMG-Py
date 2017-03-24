28b1ac2ce8c7ab32197b5dd64f8510631ee0e1f7
Max Liu <mjliu@mit.edu>
2017-03-24 18:30:20 -0400

    Add unit test for reacting benzene bonds to familyTest
    Also change database loading to class method

--- rmgpy/data/kinetics/familyTest.py
+++ rmgpy/data/kinetics/familyTest.py
@@ -1,22 +1,24 @@
 import unittest
-from rmgpy.data.kinetics.database import KineticsDatabase
 import os.path
-from rmgpy.molecule.group import Group
+
 from rmgpy import settings
+from rmgpy.data.kinetics.database import KineticsDatabase
+from rmgpy.data.kinetics.family import ReactionRecipe
+from rmgpy.molecule import Molecule
 ###################################################
 
 class TestFamily(unittest.TestCase):
 
-
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         """
-        A function run before each unit test in this class.
+        A function run ONCE before all unit tests in this class.
         """
         # Set up a dummy database
-        self.database = KineticsDatabase()
-        self.database.loadFamilies(os.path.join(settings['test_data.directory'], 'testing_database/kinetics/families'),
-                                   families=['intra_H_migration'])
-        self.family = self.database.families['intra_H_migration']
+        cls.database = KineticsDatabase()
+        cls.database.loadFamilies(os.path.join(settings['test_data.directory'], 'testing_database/kinetics/families'),
+                                  families=['intra_H_migration', 'R_Addition_MultipleBond'])
+        cls.family = cls.database.families['intra_H_migration']
 
     def testGetBackboneRoots(self):
         """
@@ -42,3 +44,43 @@ class TestFamily(unittest.TestCase):
         self.assertEquals(len(topGroups), 2)
         self.assertIn(self.family.groups.entries["R5Hall"], topGroups)
         self.assertIn(self.family.groups.entries["R6Hall"], topGroups)
+
+    def testReactBenzeneBond(self):
+        """
+        Test that benzene bonds can be properly reacted.
+        """
+        family = self.database.families['R_Addition_MultipleBond']
+        reactants = [Molecule().fromAdjacencyList("""
+1  *1 C u0 p0 c0 {2,B} {6,B} {7,S}
+2  *2 C u0 p0 c0 {1,B} {3,B} {8,S}
+3     C u0 p0 c0 {2,B} {4,B} {9,S}
+4     C u0 p0 c0 {3,B} {5,B} {10,S}
+5     C u0 p0 c0 {4,B} {6,B} {11,S}
+6     C u0 p0 c0 {1,B} {5,B} {12,S}
+7     H u0 p0 c0 {1,S}
+8     H u0 p0 c0 {2,S}
+9     H u0 p0 c0 {3,S}
+10    H u0 p0 c0 {4,S}
+11    H u0 p0 c0 {5,S}
+12    H u0 p0 c0 {6,S}
+"""),
+                     Molecule().fromAdjacencyList("1 *3 H u1 p0 c0")]
+        expectedProduct = Molecule().fromAdjacencyList("""
+1  C u0 p0 c0 {2,S} {6,S} {7,S} {13,S}
+2  C u1 p0 c0 {1,S} {3,S} {8,S}
+3  C u0 p0 c0 {2,S} {4,D} {9,S}
+4  C u0 p0 c0 {3,D} {5,S} {10,S}
+5  C u0 p0 c0 {4,S} {6,D} {11,S}
+6  C u0 p0 c0 {1,S} {5,D} {12,S}
+7  H u0 p0 c0 {1,S}
+8  H u0 p0 c0 {2,S}
+9  H u0 p0 c0 {3,S}
+10 H u0 p0 c0 {4,S}
+11 H u0 p0 c0 {5,S}
+12 H u0 p0 c0 {6,S}
+13 H u0 p0 c0 {1,S}
+""")
+        products = family.applyRecipe(reactants)
+
+        self.assertEqual(len(products), 1)
+        self.assertTrue(expectedProduct.isIsomorphic(products[0]))
